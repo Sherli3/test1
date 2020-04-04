b61ef4cddd5314226e5985130c2a5be6be20743e
Anastasiya Klimkova <anastasiya.klimkovaa@gmail.com>
2020-04-04 23:04:04 +0300

    gkl

--- src/com/anastasiya/Just.java
+++ src/com/anastasiya/Just.java
@@ -1,4 +1,24 @@
 package com.anastasiya;
 
 public class Just {
+
+    private String s;
+    private int a;
+    private int ab;
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
 }
